f54161c7fc66628e98e2202464856b16883a05da
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-08-29 11:07:54 +0900

    secrets.yml に記述されている Docomo API Key をハッシュDocomo以下に変更

--- app/services/word_service/base.rb
+++ app/services/word_service/base.rb
@@ -1,6 +1,6 @@
 module WordService
   module Base
-    API_KEY = Rails.application.secrets.docomo_api_key
+    API_KEY = Rails.application.secrets.docomo['api_key']
     API_BASE_URL = 'https://api.apigw.smt.docomo.ne.jp'.freeze
   end
 end
