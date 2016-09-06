9e6af34901a49c031425e58d1840c72dade08591
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-09-07 07:48:58 +0900

    root を images#new に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-
-  resources :images, only:[ :show, :new, :index, :create ] do
+  root 'images#new'
+  resources :images, only: [:show, :new, :index, :create] do
     member do
       get 'data'
     end
