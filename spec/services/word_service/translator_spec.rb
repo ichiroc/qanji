384593fdb0d1feaed4061a7c88b4dd282fc7317e
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-09-07 07:44:41 +0900

    word_id の取得の仕方を変更

--- spec/services/word_service/translator_spec.rb
+++ spec/services/word_service/translator_spec.rb
@@ -1,21 +1,18 @@
 # coding: utf-8
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe WordService::Translator do
-  before do
-    # word は固定したいので(毎回作りたくない)インスタンス変数にする
-    @word_id = Word.create(text: '山', score: '1.0', image: image).id
-  end
   let(:file_path) { "#{Rails.root}/db/seed_data/sample.jpg" }
   let(:image) do
     Image.create(file_name: File.basename(file_path),
                  data: File.new(file_path, 'rb').read,
                  content_type: 'image/jpeg')
   end
-  let(:word) { Word.find(@word_id) }
+  let(:word_id) { Word.find_or_create_by(text: '山', score: '1.0', image: image).id }
   let(:translated_text) do
-    WordService::Translator.new(@word_id).execute
-    word.english
+    WordService::Translator.new(word_id).execute
+    Word.find(word_id).english
   end
 
   describe '#execute' do
