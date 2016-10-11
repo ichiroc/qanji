2c6f13173f6272aa9414f0bbf5970c12d3697f8e
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-10-11 21:28:25 +0900

    words.json が空になるバグを修正

--- app/assets/javascripts/image.jsx
+++ app/assets/javascripts/image.jsx
@@ -3,10 +3,7 @@ $(window).on("load",
                  var img = new Image();
                  img.src = location.pathname + "/data";
                  $(img).on("load", () => {
-                     var canvas = document.getElementById("canvas");
-                     canvas.width = img.width;
-                     canvas.height = img.height;
-                     var ctx = canvas.getContext("2d");
+                     var ctx = document.getElementById("canvas").getContext("2d");
                      ctx.drawImage(img,0,0);
                      ReactDOM.render(
                          <WordList />,
